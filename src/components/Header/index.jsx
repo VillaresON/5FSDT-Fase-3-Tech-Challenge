c5b9cf6673d22f04b69438e2f676416d5ac70ac4
Jonathas Villares <113390493+VillaresON@users.noreply.github.com>
2025-09-17 16:48:32 -0300

    Update

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,6 +2,7 @@ import { useContext } from "react"
 import { AuthContext } from "../../context/AuthContext"
 import { useNavigate } from "react-router-dom"
 import styles from './Header.module.css'
+import { ImExit } from "react-icons/im"
 
 export default function Header() {
   const { user, logout } = useContext(AuthContext)
@@ -16,9 +17,9 @@ export default function Header() {
     <header className={styles.divPrincipal} >
       <div><b>Bem-vindo,</b></div>
       <p>{user?.nome || user?.email}</p>
-      <button>
-        Logout
-      </button>
+        <button>
+          <ImExit /> Logout
+        </button>
     </header>
   )
 }
